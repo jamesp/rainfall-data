547f34279afa50e777fa923683f1fea5e8a38b01
James Penn <james@jamespenn.co.uk>
2023-03-31 20:31:22 +0100

    single_file.py created

---
+++ src/single_file.py
@@ -0,0 +1,28 @@
+"""
+Convert all daily data into a single csv file
+"""
+
+import sys
+from pathlib import Path
+import csv
+
+base_path: Path = Path(__file__).parent.parent
+
+data_dir = base_path / "data"
+outfile = sys.argv[1]
+
+with open("output.csv", "w") as fh:
+    writer = csv.writer(fh)
+    header = ['time', 'rainfall_mm', 'site']
+    writer.writerow(header)
+    for site in data_dir.iterdir():
+        loc = site.name
+        f = site/"daily.csv"
+        with open(site/"daily.csv") as inp:
+            reader = csv.reader(inp)
+            header = reader.__next__()
+            writer.writerows((*r, loc) for r in reader)
+        
+
+    
+
